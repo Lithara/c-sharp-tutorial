fe71b9ef9db96adb837e76dccdeefbe185ddc6f0
Lithara <litharaperera2002@gmail.com>
2023-02-22 21:34:53 +0530

    05

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace ConsoleApp2
 {
@@ -11,7 +13,60 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int[,] arr = new int[2, 4];
+            Random random = new Random();
+
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            String response;
+            
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while (guess != number)
+                {
+                    Console.WriteLine("Guess a number between " + min + "-" + max + ": ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess : " + guess);
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess + " is too high");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + " is too low");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("Number : " + number);
+                Console.WriteLine("YOU WIN!");
+                Console.WriteLine("Guesses : " + guesses);
+
+                Console.WriteLine("Would you like to play again? (Y/N) : ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+            }
+
+            Console.WriteLine("Thanks for playing! ... I guess");
+            
+            /*int[,] arr = new int[2, 4];
 
             for (int i = 0; i < 2; i++)
             {
@@ -29,7 +84,7 @@ namespace ConsoleApp2
                     Console.Write(arr[i, j]);
                 }
                 Console.WriteLine("");
-            }
+            }*/
             
             
             /*int[] numbers = new int[10];
