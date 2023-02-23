dcfcde4ad07c561b46548acd7a9d46659e559ea8
Lithara <litharaperera2002@gmail.com>
2023-02-24 02:55:29 +0530

    09

---
+++ Console1/Console1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string name = "Lithara Perera";
+            int age = 20;
+            double batch = 22.1;
+            Console.WriteLine("Hi" + name);
+            Console.WriteLine("You are" + age + "years old.");*/
+
+           
+            for(int i=1; i<=5; i++)
+            {
+                Console.WriteLine(i*20);
+            }
+            Console.ReadLine();
+        }
+    }
+}
