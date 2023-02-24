c9a34a5f4310f707771d2de6c568d7212a530add
Lithara <litharaperera2002@gmail.com>
2023-02-24 16:27:40 +0530

    13

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = CheckOut(3.99, 5.75, 15);
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+
+        static double CheckOut(params double[] prices)
+        {
+            double total = 0;
+
+            foreach (double price in prices)
+            {
+                total += price;
+            }
+
+            return total;
+        }
+    }
+}
